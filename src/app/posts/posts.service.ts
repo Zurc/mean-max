be7d7ae3927bbcbf6e56c3a6fd986d5025af1b06
Zurc <cruzjurado@gmail.com>
2019-03-29 14:47:40 +0000

    inject postsService to the whole app and in the constructor of post list

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,8 @@
 import { Post } from './post.model';
+import { Injectable } from '@angular/core';
 
+// this is another way of provide a service on the whole app, instead of in the app.module
+@Injectable({providedIn: 'root'})
 export class PostsService {
   // add private so you cannot edit from the outside of this service
   private posts: Post[] = [];
