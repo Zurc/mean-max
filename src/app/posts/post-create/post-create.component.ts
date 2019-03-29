3b2ad43aa04a780c59ebd77022dd8d71b656e51f
Zurc <cruzjurado@gmail.com>
2019-03-29 10:05:55 +0000

    output event emitter to make angular aware of that

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -8,7 +8,7 @@ import { Component, EventEmitter } from '@angular/core';
 export class PostCreateComponent {
   enteredTitle = '';
   enteredContent = '';
-  postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
     const post = { title: this.enteredTitle, content: this.enteredContent }
