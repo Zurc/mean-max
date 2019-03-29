b2501b262f6e49beca8eaedb514cf53e004c2f0f
Zurc <cruzjurado@gmail.com>
2019-03-29 09:57:49 +0000

    conditionally render posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,11 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit {
-  posts: [] = [
-    {title: 'First Post', content: 'This is the first post content'},
-    {title: 'Second Post', content: 'This is the second post content'},
-    {title: 'Third Post', content: 'This is the third post content'},
-  ]
+  posts: [] = []
   constructor() { }
 
   ngOnInit() {
