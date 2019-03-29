02fd52773f3f76bb006fb055e4902b6fd22a9b4c
Zurc <cruzjurado@gmail.com>
2019-03-29 18:43:09 +0000

    sort CORS error on app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*"); // any domain allowed to access our resources
+  res.setHeader(
+    "Access-Control-Allow-Headers", // restrict to certain domains
+    "Origin, X-Requested-With, Content-Type, Accept"  // the incoming request might have this headers
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  next();
+})
+
 app.use('/api/posts',(req, res, next) => {
   const posts = [
     {
