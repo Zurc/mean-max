3c660f423ed21427b6407d01762640d801a7fa0c
Zurc <cruzjurado@gmail.com>
2019-03-29 15:10:09 +0000

    implement communication btwn comps with Subject and PostsService

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  @Input() posts: Post[] = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      // this subscription doesn't cancel when the component is not more part of the DOM, so we need to do it manually
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,11 +1,13 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // this is another way of provide a service on the whole app, instead of in the app.module
 @Injectable({providedIn: 'root'})
 export class PostsService {
   // add private so you cannot edit from the outside of this service
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
 
   // retrieve posts
   getPosts() {
@@ -13,8 +15,14 @@ export class PostsService {
     return [...this.posts];
   }
 
+  // listen to the subject (after we add any new post with addPost method)
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPost(post: Post) {
     const newPost: Post = post;
     this.posts.push(newPost);
+    this.postsUpdated.next([...this.posts]);  // how Subjects emit in this case a new copy of my posts after I updated them
   }
 }
