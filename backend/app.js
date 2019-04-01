5555860e0fb246203201fcec856e4771a237bc2a
Zurc <cruzjurado@gmail.com>
2019-04-01 11:55:00 +0100

    stored data on the database, setup on the cloud, check on shell

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://cruz:XTcvkR8hRGu2f5UJ@cluster0-595il.mongodb.net/test?retryWrites=true')
+mongoose.connect('mongodb+srv://cruz:XTcvkR8hRGu2f5UJ@cluster0-595il.mongodb.net/node-angular?retryWrites=true')
   .then(() => {
     console.log('Connected to database!');
   })
@@ -33,7 +33,7 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   })
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
