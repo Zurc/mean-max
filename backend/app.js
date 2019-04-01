df47c02607903beb261c1c4843bd98201648b836
Zurc <cruzjurado@gmail.com>
2019-04-01 10:10:51 +0100

    connecting node express to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://cruz:XTcvkR8hRGu2f5UJ@cluster0-595il.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
