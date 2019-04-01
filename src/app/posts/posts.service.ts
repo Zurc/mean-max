08ed75a2068c8e64815bac55cf8e170c126010ec
Zurc <cruzjurado@gmail.com>
2019-04-01 12:15:53 +0100

    transforming response data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 // this is another way of provide a service on the whole app, instead of in the app.module
 @Injectable({providedIn: 'root'})
@@ -14,9 +15,25 @@ export class PostsService {
 
   // retrieve posts from the backend
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    // http on angular uses observables
+    // because the id coming is _id then we use any instead of Post[] on the type
+    this.http.get<{message: string, posts: any }>('http://localhost:3000/api/posts')
+      // convert the data coming from the server before using on subscribe
+      // observables offer operators (functions) to apply to the data coming from the observable stream
+      // pipe allows as to add operators
+      .pipe(map((postData) => {
+        // convert every post, we use the normal JS map method, return this new array
+        return postData.posts.map(post => {
+          // transforming response data, return a new JS obj for every element on the array of posts
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts=> {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
