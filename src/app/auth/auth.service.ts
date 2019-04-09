38fb382ad75684bf4be5f3c5dcfc454e30afeef8
Zurc <cruzjurado@gmail.com>
2019-04-09 21:14:05 +0100

    improved UI header to reflect authentication status

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private token: string;
+  // we'll use a subject to push auth status to components interested
+  private authStatusListener = new Subject<boolean>();
 
   constructor(private http: HttpClient) { }
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    // return this as observable so we can emit changes and listen from the rest of the app
+    return  this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -27,6 +35,8 @@ export class AuthService {
     this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
       .subscribe(response => {
         this.token = response.token;
+        // emit true when the user is authenticated
+        this.authStatusListener.next(true);
       });
   }
 }
