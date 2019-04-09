c3e9cc7e5891f63663d6c9fef8e41ca42cab069d
Zurc <cruzjurado@gmail.com>
2019-04-09 13:52:47 +0100

    register new user on signup, router

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
 
 const User = require('../models/user');
 
@@ -6,11 +7,26 @@ const router = express.Router();
 
 // register routes
 router.post("/signup", (req, res, next) => {
-  // get email and pass from the incoming request
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password  // storing this unencrypted is BAD idea
-  })
+  // hash the pass for security
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(200).json({
+            message: 'User created!',
+            result
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          })
+        });
+    });
 });
 
 // export router
